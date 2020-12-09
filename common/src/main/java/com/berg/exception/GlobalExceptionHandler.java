4ce64b8eeef387465a065e0266da1fc653cb9fcb
libg <664120843@qq.com>
2020-12-09 13:48:19 +0800

    no message

--- common/src/main/java/com/berg/exception/GlobalExceptionHandler.java
+++ common/src/main/java/com/berg/exception/GlobalExceptionHandler.java
@@ -24,14 +24,14 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex) {
 		Result result = getResult(ex,true);
-		return new ResponseEntity<Object>(result, HttpStatus.OK);
+		return new ResponseEntity<Object>(result, result.getHttpStatus());
 	}
 
 	public Result getResult(Throwable ex,Boolean islog){
 		Result result;
 		if (ex instanceof AppException) {
 			AppException e = (AppException) ex;
-			result = new Result(e.getErrorCode(), e.getErrorMsg(), null);
+			result = new Result(e.getErrorCode(), e.getErrorMsg(), null,HttpStatus.OK);
 			if(islog){
 				log.error(e.getErrorMsg());
 			}
@@ -45,7 +45,7 @@ public class GlobalExceptionHandler {
 			list.forEach(item -> {
 				errList.add(item.getDefaultMessage());
 			});
-			result = new Result(MessageConstant.PARAMETER_ERROR_CODE, StringUtils.join(errList, ";"), null);
+			result = new Result(MessageConstant.PARAMETER_ERROR_CODE, StringUtils.join(errList, ";"), null,HttpStatus.BAD_REQUEST);
 		} else if (ex instanceof BindException) {
 			if(islog) {
 				log.error("请求参数错误", ex);
@@ -56,12 +56,12 @@ public class GlobalExceptionHandler {
 			list.forEach(item -> {
 				errList.add(item.getDefaultMessage());
 			});
-			result = new Result(MessageConstant.PARAMETER_ERROR_CODE, StringUtils.join(errList, ";"), null);
+			result = new Result(MessageConstant.PARAMETER_ERROR_CODE, StringUtils.join(errList, ";"), null,HttpStatus.BAD_REQUEST);
 		}else {
 			if(islog) {
 				log.error("运行异常", ex);
 			}
-			result = new Result(MessageConstant.SYSTEM_ERROR_CODE, "运行异常", ex.getMessage());
+			result = new Result(MessageConstant.SYSTEM_ERROR_CODE, "运行异常", ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		return result;
 	}
